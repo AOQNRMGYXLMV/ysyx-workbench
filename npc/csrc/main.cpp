ede80f913de47d9811a4a37b4380772e1f0216ef
SiYuhan <489365227@qq.com>
2025-03-07 16:59:36 +0800

    添加双控开关并记录波形

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
-int main() {
-  printf("Hello, ysyx!\n");
+#include "Vtop.h"
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+
+int main(int argc, char** argv) {
+  VerilatedContext* contextp = new VerilatedContext;
+  contextp->commandArgs(argc, argv);
+  Vtop* top = new Vtop{contextp};
+
+  // 启动波形追踪
+  Verilated::traceEverOn(true);
+  VerilatedVcdC* tfp = new VerilatedVcdC;
+  top->trace(tfp, 99);
+  tfp->open("waveform.vcd");
+
+  unsigned int main_time = 0;
+  while (!contextp->gotFinish() && main_time <= 200) {
+    int a = rand() & 1;
+    int b = rand() & 1;
+    top->a = a;
+    top->b = b;
+    top->eval();
+    tfp->dump(main_time++);
+
+    printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+    assert(top->f == (a ^ b));
+  }
+
+  tfp->close();
+  delete top;
+  delete contextp;
+
   return 0;
 }
